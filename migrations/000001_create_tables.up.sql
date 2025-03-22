b0505a0d11562ea37d0fde570024ac8b69b0bd15
eliotbyte <eliotbyte+github@proton.me>
2025-03-22 23:15:57 +0300

    feat: initial database schema

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -5,6 +5,47 @@ CREATE TABLE users (
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO role (name) VALUES ('owner'), ('guest');
+
+CREATE TABLE space (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    owner_id INTEGER REFERENCES users(id),
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE user_to_space (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    space_id INTEGER NOT NULL REFERENCES space(id),
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    UNIQUE (user_id, space_id)
+);
+
+CREATE TABLE topic_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO topic_type (name) VALUES ('diary'), ('dashboard');
+
+CREATE TABLE topic (
+    id SERIAL PRIMARY KEY,
+    space_id INTEGER NOT NULL REFERENCES space(id),
+    name VARCHAR(255) NOT NULL,
+    type_id INTEGER NOT NULL REFERENCES topic_type(id),
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
 CREATE TABLE tag (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL
@@ -19,7 +60,8 @@ CREATE TABLE note (
     date TIMESTAMP NOT NULL DEFAULT NOW(),
     parent_id INTEGER REFERENCES note(id),
     reply_count INTEGER DEFAULT 0,
-    is_deleted BOOLEAN DEFAULT FALSE
+    is_deleted BOOLEAN DEFAULT FALSE,
+    topic_id INTEGER REFERENCES topic(id)
 );
 
 CREATE TABLE note_to_tag (
--- migrations/000002_create_spaces_topics_roles.down.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE note DROP COLUMN topic_id;
-
-DROP TABLE topic;
-DROP TABLE topic_type;
-DROP TABLE user_to_space;
-DROP TABLE space;
-DROP TABLE role;
